8e27c75e8b5dfdd3379e8d96a2be9b868e4c3302
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2025-01-27 15:08:13 -0500

    added Models - Will update order model

--- backend/ecom/store/admin.py
+++ backend/ecom/store/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin # type: ignore
+from .models import Customer, Category, Product, Order
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
